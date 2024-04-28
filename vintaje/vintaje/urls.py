9cddd24f48d6bd4951cd2e21b10d6c58351322c2
gabi <gabinelepcu29@gmail.com>
2024-04-28 15:06:05 +0200

    update

--- vintaje/vintaje/urls.py
+++ vintaje/vintaje/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     
     path('about/',about,name = 'about'),
     path('dashboard/',include('dashboard.urls')),
+    path("inbox/", include('conversation.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
