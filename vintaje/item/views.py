aabcc889d29001289961d9c12026a6e9b85d5e0a
gabi <gabinelepcu29@gmail.com>
2024-04-28 14:18:07 +0200

    update

--- vintaje/item/views.py
+++ vintaje/item/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from .models import Item
+from .models import Item,Category
+from django.db.models import Q
 
 from django.contrib.auth.decorators import login_required
-from .forms import NewItemForm
+from .forms import NewItemForm,EditItemForm
+
+
+def items(request):
+    query=request.GET.get('query','')
+    category_id=request.GET.get('category',None)
+    categories= Category.objects.all()
+    items=Item.objects.filter(is_sold=False)
+
+
+    if category_id:
+        category_id=int(category_id)
+        items=items.filter(category_id=category_id)
+    if query:
+        items=items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request,'item/items.html',{
+        'items':items,
+        'query':query,
+        'categories':categories,
+        'category_id':category_id
+    })
+
 # Create your views here. pk=primarykey
 def detail(request,pk):    
     item=get_object_or_404(Item,pk=pk)
@@ -31,6 +54,22 @@ def new(request):
     return render(request,'item/form.html',{
         'form':form,
         'title':'Obiect nou'
+    })
+def edit(request,pk):
+    item=get_object_or_404(Item,pk=pk,created_by=request.user)
+    if request.method =='POST':
+        form=EditItemForm(request.POST,request.FILES,instance=item)
+
+        if form.is_valid():
+           form.save()
+
+           return redirect('item:detail',pk=item.id)
+    else:
+        form=EditItemForm(instance=item)
+
+    return render(request,'item/form.html',{
+        'form':form,
+        'title':'Obiect editat'
     })
 
 @login_required
